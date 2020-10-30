d18028b40f65028c64be060c5e64895638e3f169
Elson Júnior <elson.araujo.bw@gmail.com>
2020-10-30 16:20:31 -0400

    Adição cors acesso a api e multer up files

--- src/routes/locations.routes.ts
+++ src/routes/locations.routes.ts
@@ -1,7 +1,46 @@
 import { Router } from 'express';
+import multer from 'multer';
 import knex from '../database/connection';
+import multerConfig from '../helpers/multer';
 
 const locationsRoutes = Router();
+
+const upload = multer(multerConfig);
+
+locationsRoutes.get('/', async (request, response) => {
+  const { city, uf, items } = request.query;
+
+  const parsedItems = <any>String(items)
+    .split(',')
+    .map((item) => Number(item.trim()));
+
+  const locations = await knex('locations')
+    .join('location_items', 'locations.id', '=', 'location_items.location_id')
+    .whereIn('location_items.item_id', parsedItems)
+    .where('city', String(city))
+    .where('uf', String(uf))
+    .distinct()
+    .select('locations.*');
+
+  return response.json(locations);
+});
+
+locationsRoutes.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const location = await knex('locations').where('id', id).first();
+
+  if (!location) {
+    return response.status(400).json({ message: 'Location not found!' });
+  }
+
+  const items = await knex('items')
+    .join('location_items', 'items.id', '=', 'location_items.item_id')
+    .where('location_items.location_id', id)
+    .select('items.title');
+
+  return response.json({ location, items });
+});
 
 locationsRoutes.post('/', async (request, response) => {
   const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
@@ -23,8 +62,10 @@ locationsRoutes.post('/', async (request, response) => {
 
   const location_id = newIds[0];
 
-  const locationItems = items.map((item_id: number) => {
-    if (!transaction('items').where('id', item_id).first()) {
+  const locationItems = items.map(async (item_id: number) => {
+    const selectedItem = await transaction('items').where('id', item_id).first();
+
+    if (!!selectedItem) {
       return response.status(400).json({ message: 'Item not found.' });
     }
     return {
@@ -43,4 +84,21 @@ locationsRoutes.post('/', async (request, response) => {
   });
 });
 
+locationsRoutes.put('/:id', upload.single('image'), async (request, response) => {
+  const { id } = request.params;
+
+  const image = request.file.filename;
+
+  const location = await knex('locations').where('id', id).first();
+
+  if (!location) {
+    return response.status(400).json({ message: 'Location not found!' });
+  }
+
+  const locationUpdate = { ...location, image };
+
+  await knex('locations').update(locationUpdate).where('id', id);
+
+  return response.json(locationUpdate);
+});
 export default locationsRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
 const app = express();
+app.use(
+  cors({
+    // origin: ['jazbewor.com', 'zanmer.com.br'],
+  })
+);
 
 app.use(express.json());
 app.use(routes);
